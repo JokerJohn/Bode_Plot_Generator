2b7108e2868c478bc962c6df61afa87bd700be12
Evan Schimmel <evanrschimmel@gmail.com>
2021-10-26 23:11:11 -0400

    Complete rehaul
    Should work for all orders of equations through unwrap command, should mimic paper in x axis and scale to min/max in y axis.

--- Bode_Plot_Generator.m
+++ Bode_Plot_Generator.m
@@ -12,55 +12,56 @@ stepsize = 0.1;
 i=0;
 
 % Loop over frequencies
-for omega = 0.1:stepsize:1000
+for omega = 10e-2:stepsize:10e2
     i=i+1;
     s = omega*1i;
     
     % **** ADD YOUR TRANSFER FUNCTION HERE ****
-    TF_numer = s+10;
-    TF_denom = s^2+0.6*s+1;
+    numer_TF = 100*(s+1);
+    denom_TF = (s^2+110*s+1000);
     % **** ADD YOUR TRANSFER FUNCTION HERE ****
     
     % Calculations for plots
-    freq(i) = omega;
-    re_numer = real(TF_numer);
-    im_numer = imag(TF_numer);
-    re_denom = real(TF_denom);
-    im_denom = imag(TF_denom);
-    FRF_mag = (sqrt((re_numer)^2+(im_numer)^2)) / (sqrt((re_denom)^2+(im_denom)^2));
+    omega_val(i) = omega;
+    numer_re = real(numer_TF);
+    numer_im = imag(numer_TF);
+    denom_re = real(denom_TF);
+    denom_im = imag(denom_TF);
+    FRF_mag = (sqrt((numer_re)^2+(numer_im)^2)) / (sqrt((denom_re)^2+(denom_im)^2));
     DB_mag(i) = 20*log10(abs(FRF_mag));
-    numer_phase = atand(im_numer/re_numer);
-    denom_phase = atand(im_denom/re_denom);
-    if im_numer > 0 && re_numer < 0
-        numer_phase = 180 + numer_phase;
-    elseif im_denom > 0 && re_denom < 0
-        denom_phase = 180 + denom_phase;
-    elseif im_numer < 0 && re_numer < 0
-        numer_phase = 180 + numer_phase;
-    elseif im_denom < 0 && re_denom < 0
-        denom_phase = 180 + denom_phase;
-    end
-    FRF_phase(i) = numer_phase - denom_phase;
+    numer_phase_rad = atan2(numer_im,numer_re);
+    denom_phase_rad = atan2(denom_im,denom_re);
+    FRF_phase_rad(i) = numer_phase_rad - denom_phase_rad;
 end
+
+FRF_phase_deg = (360/(2*pi)) * unwrap(FRF_phase_rad);
 
 % Create magnitude and phase plots
 figure
 set(gcf, 'color', 'w')
+
 subplot(2,1,1)
-plot(freq,DB_mag,'k-')
+plot(omega_val,DB_mag,'k-')
 set(gca, 'XScale','log');
 xlabel('Frequency [rad/s]')
 ylabel('Magnitude [dB]')
-axis([0.1 1000 -40 40])
-xticks([10e-1 10e0 10e1 10e2 10e3]);
-yticks([-40 -30 -20 -10 0 10 20 30 40]);
+xlim([10e-2 10e2])
+ymin = min(DB_mag)-20;
+ymax = max(DB_mag)+20;
+ylim([ymin ymax])
+xticks([10e-2 10e-1 10e0 10e1 10e2]);
+yticks([-100:20:100]);
 grid
+
 subplot(2,1,2)
-plot(freq,FRF_phase,'k-')
+plot(omega_val,FRF_phase_deg,'k-')
 set(gca, 'XScale','log');
 xlabel('Frequency [rad/s]')
 ylabel('Phase [deg]')
-axis([0.1 1000 -180 180])
-xticks([10e-1 10e0 10e1 10e2 10e3]);
-yticks([-180 -135 -90 -45 0 45 90 135 180]);
+xlim([10e-2 10e2])
+ymin = min(FRF_phase_deg)-22.5;
+ymax = max(FRF_phase_deg)+22.5;
+ylim([ymin ymax])
+xticks([10e-2 10e-1 10e0 10e1 10e2]);
+yticks([-360:45:360]);
 grid
